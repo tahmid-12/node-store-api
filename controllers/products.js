4fe907bd3ec32adc212674dc540af514ebc6df45
Md. Tahmid Chowdhury <mdtahmidchowdhury12@gmail.com>
2022-08-02 19:53:35 +0600

    mongose query

--- controllers/products.js
+++ controllers/products.js
@@ -1,10 +1,26 @@
+const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-    res.status(200).json({ msg: "Get All Products"});
+    // const products = await Product.find({ 
+    //     featured: true,
+    // })
+    const products = await Product.find({}).sort('-name')
+    res.status(200).json({ products, nbHits: products.length });
 }
 
 const getAllProducts = async (req, res) => {
-    res.status(200).json({ msg: "Get a Single Product" });
+    // console.log(req.query)
+    const { featured,company } = req.query;
+    queryObject = {};
+    if(featured){
+        queryObject.featured = featured === 'true' ? true : false
+    }
+    if(company){
+        queryObject.company = company
+    }
+    console.log(queryObject)
+    const products = await Product.find(queryObject);
+    res.status(200).json({ products, nbHits: products.length });
 }
 
 module.exports = {
